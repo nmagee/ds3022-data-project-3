16947461b4d3054884215570d562d055970312f0
AnnaYao <annayao@Annas-MacBook-Air-6.local>
2025-11-20 19:53:31 -0500

    edits to display

--- display.py
+++ display.py
@@ -3,6 +3,9 @@ from dash import dcc, html
 from dash.dependencies import Input, Output
 import os
 from quixstreams import Application
+import duckdb
+import plotly.express as px
+import pandas as pd
 
 KAFKA_BROKER = os.getenv("KAFKA_BROKER", "127.0.0.1:19092,127.0.0.1:29092,127.0.0.1:39092")
 TOPIC_NAME = "github-commits"
@@ -22,7 +25,6 @@ except Exception as e:
     print(f"Kafka connection failed: {e}")
     consumer = None
 
-TOTAL_COMMITS = 0
 
 app = dash.Dash(__name__)
 app.layout = html.Div([
@@ -37,34 +39,58 @@ app.layout = html.Div([
     }),
     html.Div("Commits Processed", style={'text-align': 'center', 'font-size': '24px', 'color': '#555'}),
 
+    dcc.Graph(id='activity-graph'),
+
     # Update every 1 second (1000ms)
     dcc.Interval(id='interval-component', interval=1000, n_intervals=0)
 ])
 
 @app.callback(
-    Output('counter-display', 'children'),
+    [Output('counter-display', 'children'),
+     Output('activity-graph', 'figure')],
     [Input('interval-component', 'n_intervals')]
 )
-def update_counter(n):
-    global TOTAL_COMMITS
-    
-    if consumer:
+def update_dashboard(n):
+    try:
+        conn = duckdb.connect("commits.db, read_only=True")
+        df = conn.execute("""
+            SELECT 
+                date,
+                count(*) AS commit_count
+            FROM commits
+            GROUP BY date
+            ORDER BY date ASC
+                          """).df()
         
-        for _ in range(50):
-            msg = consumer.poll(0.01) 
-            
-            if msg is None:
-                break 
-            
-            if msg.error():
-                print("Kafka Error:", msg.error())
-                continue
-            
-            TOTAL_COMMITS += 1
-            
-            consumer.store_offsets(msg)
+        conn.close()
 
-    return f"{TOTAL_COMMITS}"
+        if df.empty:
+            empty_fig = px.line(title="Waiting for data...")
+            return empty_fig, "0"
+        
+        total_commits = df['commit_count'].sum()
+
+        fig = px.line(
+            df, 
+            x='date', 
+            y='commit_count', 
+            title="Commit Frequency Over Time",
+            markers=True
+        )
+        
+        fig.update_layout(
+            xaxis_title="Date", 
+            yaxis_title="Commits per Day",
+            template="plotly_white"
+        )
+        
+        return fig, f"{total_commits:,}"
+
+    except Exception as e:
+        return (f"DB Error: {e}")
+
+    
+   
 
 if __name__ == '__main__':
     app.run(debug=True, port=8050)
